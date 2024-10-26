983ade63fb075ed089e126553b6e35e3fb1c0c9f
Jasmina Savkovic <jsavkovi@gmail.com>
2024-10-26 00:51:38 -0400

    Add more weather data.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
 function updateWeather (response) {
   tempEl = document.querySelector('#temperature')
-  tempEl.innerHTML = Math.round(response.data.temperature.current)
   let cityEl = document.querySelector('#city')
+  let descriptionEl = document.querySelector('#description')
+  let humidityEl = document.querySelector('#humidity')
+  let windEl = document.querySelector('#wind')
+  let timeEl = document.querySelector('#time')
+  let date = new Date(response.data.time * 1000)
+
+  console.log(response.data)
+
+  tempEl.innerHTML = Math.round(response.data.temperature.current)
   cityEl.innerHTML = response.data.city
+  descriptionEl.innerHTML = response.data.condition.description
+  humidityEl.innerHTML = `${response.data.temperature.humidity} %`
+  windEl.innerHTML = `${response.data.wind.speed} km/h`
+  timeEl.innerHTML = formatDate(date)
+}
+
+function formatDate (date) {
+  let minutes = date.getMinutes()
+  let hours = date.getHours()
+  let days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ]
+  let day = days[date.getDay()]
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+
+  return `${day} ${hours}:${minutes}`
 }
 
 function searchCity (city) {
