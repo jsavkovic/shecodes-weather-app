a8bbfc5452da2c57822e607c8e2fdad2a64f8c8b
Jasmina Savkovic <jsavkovi@gmail.com>
2024-10-26 00:13:24 -0400

    Add API integration to get city temperature data.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+function updateWeather (response) {
+  tempEl = document.querySelector('#temperature')
+  tempEl.innerHTML = Math.round(response.data.temperature.current)
+  let cityEl = document.querySelector('#city')
+  cityEl.innerHTML = response.data.city
+}
+
+function searchCity (city) {
+  let apiKey = 'f155ba5d1ot40a88800e7016d3b9ad61'
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+
+  axios.get(apiUrl).then(updateWeather)
+}
+
 function handleSearchSubmit (e) {
   e.preventDefault()
   let searchInput = document.querySelector('#search-input')
-  let cityEl = document.querySelector('#city')
-  cityEl.innerHTML = searchInput.value
+  searchCity(searchInput.value)
 }
 
 let searchFormEl = document.querySelector('#search-form')
 searchFormEl.addEventListener('submit', handleSearchSubmit)
+
+searchCity('Paris')
